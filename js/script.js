9ea8d0c49bcd2a338c3366de9594aa0d58a0ca41
MaximeMenotti <maxime.menotti@gmail.com>
2018-03-14 10:16:35 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+(function($) {
+  $(document).ready(function() {
+    $('.accordion').click(function(evt) {
+      var pannel = $(evt.currentTarget).next();
+      if (pannel.is(':hidden')) {
+        pannel.slideDown('slow');
+        $(evt.currentTarget).toggleClass('active');
+      } else {
+        pannel.slideUp('slow');
+        $(evt.currentTarget).removeClass('active');
+      }
+    });
+
+    $('.menu a').click(function(evt) {
+      scrollToPosition(evt);
+    });
+  });
+  
+  function scrollToPosition(evt) {
+    evt.preventDefault();
+    var target = $(evt.currentTarget).attr('href');
+    console.log(target)
+    $('html, body').animate({scrollTop: $(target).offset().top}, 'slow');
+  }
+})(jQuery);
