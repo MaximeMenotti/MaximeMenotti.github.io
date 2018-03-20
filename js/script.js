aee7214de5629d936b43df55c3a546216702de25
MaximeMenotti <maxime.menotti@gmail.com>
2018-03-20 18:09:13 +0100

    Changement police et ajout variable

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
-(function($) {
-  $(document).ready(function() {
+jQuery(function($) {
     $('.accordion').click(function(evt) {
       var pannel = $(evt.currentTarget).next();
       if (pannel.is(':hidden')) {
         pannel.slideDown('slow');
-        $(evt.currentTarget).toggleClass('active');
+        $(evt.currentTarget).parent().toggleClass('active');
       } else {
         pannel.slideUp('slow');
-        $(evt.currentTarget).removeClass('active');
+        $(evt.currentTarget).parent().removeClass('active');
       }
     });
 
     $('.menu a').click(function(evt) {
       scrollToPosition(evt);
     });
-  });
   
-  function scrollToPosition(evt) {
-    evt.preventDefault();
-    var target = $(evt.currentTarget).attr('href');
-    console.log(target)
-    $('html, body').animate({scrollTop: $(target).offset().top}, 'slow');
-  }
+    function scrollToPosition(evt) {
+        evt.preventDefault();
+        var target = $(evt.currentTarget).attr('href');
+        console.log(target)
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 'slow');
+    }
 })(jQuery);
